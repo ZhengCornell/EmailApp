7e704b5a4131f7cd887dc415e08ab6911ba1d9f2
Zheng <zg242@cornell.edu>
2018-08-07 11:21:27 -0700

    Change name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ name: "Zheng11" });
+  res.send({ name: "Zheng1Ge1" });
 });
 
 const PORT = process.env.PORT || 5000;
